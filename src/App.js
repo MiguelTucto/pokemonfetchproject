c93fff5780b4057939ec355e37b0491b7f35507d
2CTO <u201521691@upc.edu.pe>
2023-02-27 13:48:36 -0500

    first usages for pokeAPI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/190")
+        .then(response => response.json())
+        .then(data => setPokemon(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{pokemon.order}</h1>
+      <h2>{pokemon.name}</h2>
+      <h2>{pokemon.species.url}</h2>
+      <h2>{pokemon.sprites.back_default}</h2>
+      <img src={pokemon.sprites.front_default} width="150"/>
     </div>
   );
 }
